e8455f2b69b42f1edf7305776c8e1e80d3c1a5e6
nakir323 <nakirdev@gmail.com>
2022-08-08 14:09:08 +0900

    Release v0.0.5

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,19 @@
-export function main(): void {
-  console.log("Hello, world!");
+export class UUID {
+  private value: string;
+
+  constructor() {
+    var S = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var N = 16;
+    this.value = [...Array(N)]
+      .map(() => S[Math.floor(Math.random() * S.length)])
+      .join('');
+  }
+
+  toString() {
+    return this.value;
+  }
 }
+
+export const uuid = () => {
+  return new UUID();
+};
